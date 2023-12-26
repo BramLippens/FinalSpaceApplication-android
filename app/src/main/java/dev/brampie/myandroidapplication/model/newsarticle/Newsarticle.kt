b22cf50b81f7d2263e1165d82e10f4304172cb55
BramLippens <bram.lippens09@gmail.com>
2023-12-26 20:24:42 +0100

    Created the models for newsarticle

---
+++ app/src/main/java/dev/brampie/myandroidapplication/model/newsarticle/Newsarticle.kt
@@ -0,0 +1,7 @@
+package dev.brampie.myandroidapplication.model.newsarticle
+
+class Newsarticle (
+    val sourceName: String,
+    val title: String,
+    val image: String,
+)
---
+++ app/src/main/java/dev/brampie/myandroidapplication/model/newsarticle/NewsarticleDetail.kt
@@ -0,0 +1,10 @@
+package dev.brampie.myandroidapplication.model.newsarticle
+
+data class NewsarticleDetail(
+    val sourceName: String,
+    val author: String,
+    val title: String,
+    val description: String,
+    val imageUrl: String,
+    val publishedAt: String,
+)
