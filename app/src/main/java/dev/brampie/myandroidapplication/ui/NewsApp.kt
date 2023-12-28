c2bf7c9115b9d1be500a5b23e4c9540624f02d23
BramLippens <bram.lippens09@gmail.com>
2023-12-28 15:36:55 +0100

    Navigatiebar

--- app/src/main/java/dev/brampie/myandroidapplication/ui/NewsApp.kt
+++ app/src/main/java/dev/brampie/myandroidapplication/ui/NewsApp.kt
@@ -2,86 +2,61 @@ package dev.brampie.myandroidapplication.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import dev.brampie.myandroidapplication.R
+import dev.brampie.myandroidapplication.ui.components.NavigationBar
 import dev.brampie.myandroidapplication.ui.components.NewsAppBar
+import dev.brampie.myandroidapplication.ui.navigation.NewsOverviewScreen
+import dev.brampie.myandroidapplication.ui.navigation.navComponent
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewsApp(
-    navigationType: NewsNavigationType
+    navigationType: NewsNavigationType,
+    navController: NavHostController = rememberNavController()
 ){
     val currentScreenTitle = stringResource(R.string.go_to_home)
 
+    val goHome: () -> Unit = {
+        navController.popBackStack(
+            NewsOverviewScreen.Start.name,
+            inclusive = false,
+        )
+    }
+    val goSearch: () -> Unit = {
+        navController.navigate(NewsOverviewScreen.Search.name)
+    }
+
     when(navigationType){
         NewsNavigationType.BOTTOM_NAVIGATION -> {
-            var presses by remember { mutableIntStateOf(0) }
             Scaffold(
                 topBar = {
                     NewsAppBar(currentScreentitle = currentScreenTitle)
                 },
                 bottomBar = {
-                    BottomAppBar(
-                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        contentColor = MaterialTheme.colorScheme.primary,
-                    ) {
-                        Text(
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            textAlign = TextAlign.Center,
-                            text = "Bottom app bar",
-                        )
-                    }
-                },
-                floatingActionButton = {
-                    FloatingActionButton(onClick = { presses++ }) {
-                        Icon(Icons.Default.Add, contentDescription = "Add")
-                    }
+                    NavigationBar(onHome = goHome, onNews = goSearch)
                 }
             ) { innerPadding ->
-                Column(
-                    modifier = Modifier
-                        .padding(innerPadding),
-                    verticalArrangement = Arrangement.spacedBy(16.dp),
-                ) {
-                    Text(
-                        modifier = Modifier.padding(8.dp),
-                        text =
-                        """
-                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
-
-                    It also contains some basic inner content, such as this text.
-
-                    You have pressed the floating action button $presses times.
-                """.trimIndent(),
-                    )
+                navComponent(
+                    navController = navController,
+                    modifier = Modifier.padding(innerPadding)
+                )
             }
+        }
+        NewsNavigationType.NAVIGATION_RAIL -> {
+            Text(text = "Navigation Rail")
+        }
+        NewsNavigationType.PERMANENT_NAVIGATION_DRAWER -> {
+            Text(text = "Permanent Navigation Drawer")
         }
     }
-            NewsNavigationType.NAVIGATION_RAIL -> {
-                Text(text = "Navigation Rail")
-            }
-            NewsNavigationType.PERMANENT_NAVIGATION_DRAWER -> {
-                Text(text = "Permanent Navigation Drawer")
-            }
-        }
 }
---
+++ app/src/main/java/dev/brampie/myandroidapplication/ui/navigation/Navigation.kt
@@ -0,0 +1,27 @@
+package dev.brampie.myandroidapplication.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import dev.brampie.myandroidapplication.ui.overviewScreen.NewsOverview
+
+@Composable
+fun navComponent(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = NewsOverviewScreen.Start.name,
+        modifier = modifier
+    ) {
+        composable(NewsOverviewScreen.Start.name) {
+            NewsOverview()
+        }
+        composable(NewsOverviewScreen.Search.name) {
+            NewsOverview()
+        }
+    }
+}
